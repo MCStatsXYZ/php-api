25de488b27ef87316c9aed041974d7b67201cbe5
Connor Linfoot <connorlinfoot@me.com>
2017-03-28 00:04:09 +0100

    Now inserting data into a MySQL DB

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php
+require_once( 'db.php' ); // Gives us $conn
 
-$from ='connorli@purple.enkel.hosting';
+$path = ! empty( $_GET['path'] ) ? strtolower( $_GET['path'] ) : '';
+$args = explode( '/', $path );
+if( $args[0] == 'plugin' && $args[1] == 'cratesplus' ) {
 
-// To send HTML mail, the Content-type header must be set
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- 
-// Create email headers
-$headers .= 'From: '.$from."\r\n".
-    'Reply-To: '.$from."\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-$data = json_decode( gzdecode( file_get_contents('php://input') ) );
-
-ob_start();
-
-print_r( $data );
-
-$body = ob_get_clean();
-
-if( empty( $data ) ) {
-	exit();
+	$data = json_decode( gzdecode( file_get_contents('php://input') ) );
+	if( ! empty( $data ) ) {
+		$guid = isset( $data->guid ) ? mysqli_real_escape_string( $conn, $data->guid ) : '';
+		$plugin_version = isset( $data->plugin_version ) ? mysqli_real_escape_string( $conn, $data->plugin_version ) : '';
+		$server_version = isset( $data->server_version ) ? mysqli_real_escape_string( $conn, $data->server_version ) : '';
+		$players_online = isset( $data->players_online ) ? mysqli_real_escape_string( $conn, $data->players_online ) : '';
+		$osname = isset( $data->osname ) ? mysqli_real_escape_string( $conn, $data->osname ) : '';
+		$osarch = isset( $data->osarch ) ? mysqli_real_escape_string( $conn, $data->osarch ) : '';
+		$osversion = isset( $data->osversion ) ? mysqli_real_escape_string( $conn, $data->osversion ) : '';
+		$cores = isset( $data->cores ) ? mysqli_real_escape_string( $conn, $data->cores ) : '';
+		$auth_mode = isset( $data->auth_mode ) ? mysqli_real_escape_string( $conn, $data->auth_mode ) : '';
+		$java_version = isset( $data->java_version ) ? mysqli_real_escape_string( $conn, $data->java_version ) : '';
+		$remote_ip = isset( $_SERVER['REMOTE_ADDR'] ) ? mysqli_real_escape_string( $conn, $_SERVER['REMOTE_ADDR'] ) : '';
+		
+		$sql = 'INSERT INTO `stats_cratesplus` ( `guid`, `plugin_version`, `server_version`, `players_online`, `osname`, `osarch`, `osversion`, `cores`, `auth_mode`, `java_version`, `remote_ip` ) VALUE ( "' . $guid . '", "' . $plugin_version . '", "' . $server_version . '", "' . $players_online . '", "' . $osname . '", "' . $osarch . '", "' . $osversion . '", "' . $cores . '", "' . $auth_mode . '", "' . $java_version . '", "' . $remote_ip . '" );';
+		$conn->query( $sql );
+		echo '1';
+	}
+	
 }
-
-mail( 'connor@enkel.hosting', 'MC Stats Test', $body, $headers );
